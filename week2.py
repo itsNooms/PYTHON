c8a1f605d70ed9a663aecd277b7b267f69897283
Nooman Nasir Khan <148587027+itsNooms@users.noreply.github.com>
2024-12-30 23:21:59 +0530

    Update week2.py

--- week2.py
+++ week2.py
@@ -1,7 +1,5 @@
-#Integer arithmetics
+#Last digit of integer
+a=int(input())
+print(a%10)
 
-print(17 / 3)  # gives 5.66666666667
-print(2 ** 4)  # gives 16
-print(2 ** -2) # gives 0.25
-print(17 // 3)  # gives 5
-print(17 % 3)   # gives 2
+#
